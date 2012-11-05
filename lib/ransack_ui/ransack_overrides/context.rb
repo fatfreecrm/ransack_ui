93f6bc497c602efd2e250d1d451a8e2e0512ea49
Nathan Broadbent <nathan.f77@gmail.com>
2012-11-05 23:31:24 +1300

    Overriding wrong method

--- lib/ransack_ui/ransack_overrides/context.rb
+++ lib/ransack_ui/ransack_overrides/context.rb
@@ -2,7 +2,7 @@ require 'ransack/context'
 
 module Ransack
   Context.class_eval do
-    def self.ransackable_attribute?(str, klass)
+    def ransackable_attribute?(str, klass)
       klass.ransackable_attributes(auth_object).map(&:first).include? str
     end
   end
--- lib/ransack_ui/version.rb
+++ lib/ransack_ui/version.rb
@@ -1,3 +1,3 @@
 module RansackUI
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
