809110b86bbf22434be18f8f0c682a1944258a87
Nathan Broadbent <nathan.f77@gmail.com>
2012-12-14 14:33:48 +1300

    Added support for models without associations

--- lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
+++ lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
@@ -20,15 +20,8 @@ module Ransack
             objectify_options(options), @default_options.merge(html_options)
           )
         else
-          # searchable_attributes now returns [c, type]
-          collection = object.context.searchable_attributes(bases.first).map do |c, type|
-            [
-              attr_from_base_and_column(bases.first, c),
-              Translate.attribute(attr_from_base_and_column(bases.first, c), :context => object.context)
-            ]
-          end
-          @template.collection_select(
-            @object_name, :name, collection, :first, :last,
+          @template.select(
+            @object_name, :name, attribute_collection_for_base(bases.first),
             objectify_options(options), @default_options.merge(html_options)
           )
         end
@@ -81,63 +74,65 @@ module Ransack
 
       def attribute_collection_for_bases(bases)
         bases.map do |base|
-          klass = object.context.traverse(base)
-          foreign_keys = klass.reflect_on_all_associations.select(&:belongs_to?).
-                           map_to({}) {|r, h| h[r.foreign_key.to_sym] = r.class_name }
-          ajax_options = Ransack.options[:ajax_options] || {}
-
-          if base.present?
-            model = object.context.traverse(base).model_name
-          end
-
-          begin
-          [
-            Translate.association(base, :context => object.context),
-            object.context.searchable_attributes(base).map do |c, type|
-              # Don't show 'id' column for base model
-              next nil if base.blank? && c == 'id'
-
-              attribute = attr_from_base_and_column(base, c)
-              attribute_label = Translate.attribute(attribute, :context => object.context)
-
-              # Set model name as label for 'id' column on that model's table.
-              if c == 'id'
-                foreign_klass = object.context.traverse(base).model_name
-                # Check that model can autocomplete. If not, skip this id column.
-                next nil unless foreign_klass.constantize._ransack_can_autocomplete
-                attribute_label = I18n.translate(foreign_klass, :default => foreign_klass)
-              else
-                foreign_klass = foreign_keys[c.to_sym]
-              end
-
-              # Add column type as data attribute
-              html_options = {:'data-type' => type}
-              # Set 'base' attribute if attribute is on base model
-              html_options[:'data-root-model'] = true if base.blank?
-
-              if foreign_klass
-                # If field is a foreign key, set up 'data-ajax-*' attributes for auto-complete
-                controller = foreign_klass.tableize
-                html_options[:'data-ajax-entity'] = I18n.translate(controller, :default => controller)
-                if ajax_options[:url]
-                  html_options[:'data-ajax-url'] = ajax_options[:url].sub(':controller', controller)
-                else
-                  html_options[:'data-ajax-url'] = "/#{controller}.json"
-                end
-                html_options[:'data-ajax-type'] = ajax_options[:type] || 'GET'
-                html_options[:'data-ajax-key']  = ajax_options[:key]  || 'query'
-              end
-              [
-                attribute_label,
-                attribute,
-                html_options
-              ]
-            end.compact
-          ]
-          rescue UntraversableAssociationError => e
-            nil
+          if collection = attribute_collection_for_base(base)
+            [
+              Translate.association(base, :context => object.context),
+              collection
+            ]
           end
         end.compact
+      end
+
+      def attribute_collection_for_base(base)
+        klass = object.context.traverse(base)
+        foreign_keys = klass.reflect_on_all_associations.select(&:belongs_to?).
+                         map_to({}) {|r, h| h[r.foreign_key.to_sym] = r.class_name }
+
+        ajax_options = Ransack.options[:ajax_options] || {}
+
+        object.context.searchable_attributes(base).map do |c, type|
+          # Don't show 'id' column for base model
+          next nil if base.blank? && c == 'id'
+
+          attribute = attr_from_base_and_column(base, c)
+          attribute_label = Translate.attribute(attribute, :context => object.context)
+
+          # Set model name as label for 'id' column on that model's table.
+          if c == 'id'
+            foreign_klass = object.context.traverse(base).model_name
+            # Check that model can autocomplete. If not, skip this id column.
+            next nil unless foreign_klass.constantize._ransack_can_autocomplete
+            attribute_label = I18n.translate(foreign_klass, :default => foreign_klass)
+          else
+            foreign_klass = foreign_keys[c.to_sym]
+          end
+
+          # Add column type as data attribute
+          html_options = {:'data-type' => type}
+          # Set 'base' attribute if attribute is on base model
+          html_options[:'data-root-model'] = true if base.blank?
+
+          if foreign_klass
+            # If field is a foreign key, set up 'data-ajax-*' attributes for auto-complete
+            controller = foreign_klass.tableize
+            html_options[:'data-ajax-entity'] = I18n.translate(controller, :default => controller)
+            if ajax_options[:url]
+              html_options[:'data-ajax-url'] = ajax_options[:url].sub(':controller', controller)
+            else
+              html_options[:'data-ajax-url'] = "/#{controller}.json"
+            end
+            html_options[:'data-ajax-type'] = ajax_options[:type] || 'GET'
+            html_options[:'data-ajax-key']  = ajax_options[:key]  || 'query'
+          end
+
+          [
+            attribute_label,
+            attribute,
+            html_options
+          ]
+        end.compact
+      rescue UntraversableAssociationError => e
+        nil
       end
     end
   end
