7e2808a40b1d2561f49e0bdac3f2c557da0145e1
Nathan Broadbent <nathan.f77@gmail.com>
2012-12-17 13:49:57 +1300

    Added stylesheets and button_group_select widget. Support both bootstrap and plain out of the box

--- lib/ransack_ui/view_helpers.rb
+++ lib/ransack_ui/view_helpers.rb
@@ -4,18 +4,27 @@ module RansackUI
       render 'ransack_ui/search', :options => options
     end
 
-    def link_to_add_fields(name, f, type)
+    def link_to_add_fields(name, f, type, options)
       new_object = f.object.send "build_#{type}"
       fields = f.send("#{type}_fields", new_object, :child_index => "new_#{type}") do |builder|
-        render "ransack_ui/#{type.to_s}_fields", :f => builder
+        render "ransack_ui/#{type.to_s}_fields", :f => builder, :options => options
       end
-      link_to nil, :class => "add_fields btn btn-small btn-primary", "data-field-type" => type, "data-content" => "#{fields}" do
-        "<i class=\"icon-plus-sign icon-white\"></i> #{name}".html_safe
+
+      if options[:theme].to_s == 'bootstrap'
+        link_to nil, :class => "add_fields btn btn-small btn-primary", "data-field-type" => type, "data-content" => "#{fields}" do
+          "<i class=\"icon-plus icon-white\"></i><span>#{name}</span>".html_safe
+        end
+      else
+        link_to name, nil, :class => "add_fields", "data-field-type" => type, "data-content" => "#{fields}"
       end
     end
 
-    def link_to_remove_fields(name, f)
-      link_to image_tag('ransack_ui/delete.png', :size => '16x16', :alt => name), nil, :class => "remove_fields"
+    def link_to_remove_fields(name, f, options)
+      if options[:theme].to_s == 'bootstrap'
+        link_to '<i class="icon-remove icon-white"></i>'.html_safe, nil, :class => "remove_fields btn btn-mini btn-danger"
+      else
+        link_to image_tag('ransack_ui/delete.png', :size => '16x16', :alt => name), nil, :class => "remove_fields"
+      end
     end
   end
 end
