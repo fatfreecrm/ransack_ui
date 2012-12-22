f739a66a3ccf67b900f008ccce9ae047f54f7003
Nathan Broadbent <nathan.f77@gmail.com>
2012-12-22 14:38:55 +1300

    Add assets to config.assets.precompile in before_configuration hook

--- lib/ransack_ui/rails/engine.rb
+++ lib/ransack_ui/rails/engine.rb
@@ -12,7 +12,7 @@ module RansackUI
         ActionController::Base.send :include, ControllerHelpers
       end
 
-      initializer :assets do
+      config.before_configuration do
         # Add images to be precompiled
         ::Rails.application.config.assets.precompile += %w(ransack_ui/delete.png ransack_ui/calendar.png)
       end
