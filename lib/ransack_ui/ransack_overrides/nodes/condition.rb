845ccc8b4cb9a9210c07085b63e38d44312aaa17
Mark Edmondson <mark@guestfolio.com>
2013-03-05 14:20:10 -0800

    Group all predicates, first or otherwise in order to ensure allowing "OR"

--- lib/ransack_ui/ransack_overrides/nodes/condition.rb
+++ lib/ransack_ui/ransack_overrides/nodes/condition.rb
@@ -8,6 +8,22 @@ module Ransack
       def default?
         @is_default
       end
+
+      def arel_predicate
+        predicates = attributes.map do |attr|
+          attr.attr.send(predicate.arel_predicate, formatted_values_for_attribute(attr))
+        end
+        if predicates.size > 1
+          case combinator
+          when 'and'
+            Arel::Nodes::Grouping.new(Arel::Nodes::And.new(predicates))
+          when 'or'
+            Arel::Nodes::Grouping.new(predicates.inject(&:or))
+          end
+        else
+          Arel::Nodes::Grouping.new(predicates.first)
+        end
+      end
     end
   end
 end
