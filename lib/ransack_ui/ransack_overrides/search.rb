d4d13b90fef52279d030c2fb518c9fce45b8156a
Mark Edmondson <mark@guestfolio.com>
2014-08-05 11:45:38 -0700

    Update for use with activerecord-hackery gem version

--- lib/ransack_ui/ransack_overrides/search.rb
+++ lib/ransack_ui/ransack_overrides/search.rb
@@ -3,10 +3,13 @@ require 'ransack/search'
 module Ransack
   Search.class_eval do
     def initialize(object, params = {}, options = {})
-      params ||= {}
+      params = {} unless params.is_a?(Hash)
+      (params ||= {})
+      .delete_if { |k, v| [*v].all? { |i| i.blank? && i != false } }
       @context = Context.for(object, options)
       @context.auth_object = options[:auth_object]
       @base = Nodes::Grouping.new(@context, options[:grouping] || 'and')
+      @scope_args = {}
       build(params.with_indifferent_access)
     end
   end
