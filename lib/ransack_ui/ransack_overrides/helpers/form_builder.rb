768c07cf3b3e902de58c5d8d546c4976edd24cfd
Daniel O'Connor <daniel.oconnor@gmail.com>
2019-11-08 16:24:44 +1030

    Hash syntax

--- lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
+++ lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
@@ -38,25 +38,25 @@ module Ransack
           @template.select(
             @object_name, :name,
             @template.grouped_options_for_select(attribute_collection_for_bases(bases), object.name),
-            objectify_options(options), @default_options.merge({:class => 'ransack_sort'}).merge(html_options)
+            objectify_options(options), @default_options.merge({class: 'ransack_sort'}).merge(html_options)
           ) + @template.collection_select(
             @object_name, :dir, [['asc', object.translate('asc')], ['desc', object.translate('desc')]], :first, :last,
-            objectify_options(options.except(:include_blank)), @default_options.merge({:class => 'ransack_sort_order'}).merge(html_options)
+            objectify_options(options.except(:include_blank)), @default_options.merge({class: 'ransack_sort_order'}).merge(html_options)
           )
         else
           # searchable_attributes now returns [c, type]
           collection = object.context.searchable_attributes(bases.first).map do |c, type|
             [
               attr_from_base_and_column(bases.first, c),
-              Translate.attribute(attr_from_base_and_column(bases.first, c), :context => object.context)
+              Translate.attribute(attr_from_base_and_column(bases.first, c), context: object.context)
             ]
           end
           @template.collection_select(
             @object_name, :name, collection, :first, :last,
-            objectify_options(options), @default_options.merge({:class => 'ransack_sort'}).merge(html_options)
+            objectify_options(options), @default_options.merge({class: 'ransack_sort'}).merge(html_options)
           ) + @template.collection_select(
             @object_name, :dir, [['asc', object.translate('asc')], ['desc', object.translate('desc')]], :first, :last,
-            objectify_options(options.except(:include_blank)), @default_options.merge({:class => 'ransack_sort_order'}).merge(html_options)
+            objectify_options(options.except(:include_blank)), @default_options.merge({class: 'ransack_sort_order'}).merge(html_options)
           )
         end
       end
@@ -133,7 +133,7 @@ module Ransack
         bases.map do |base|
           if collection = attribute_collection_for_base(base)
             [
-              Translate.association(base, :context => object.context),
+              Translate.association(base, context: object.context),
               collection
             ]
           end
@@ -152,7 +152,7 @@ module Ransack
               inclusions = v.send(:delimiter)
               inclusions = inclusions.call if inclusions.respond_to?(:call) # handle lambda
               hash[a.to_s] = inclusions.each_with_object({}) do |o, options|
-                options[o.to_s] = I18n.translate("activerecord.attribute_options.#{klass.to_s.downcase}.#{a}.#{o}", :default => o.to_s.titleize)
+                options[o.to_s] = I18n.translate("activerecord.attribute_options.#{klass.to_s.downcase}.#{a}.#{o}", default: o.to_s.titleize)
               end
             end
           end
@@ -176,7 +176,7 @@ module Ransack
           if column_select_options[column]
             # Format options as an array of hashes with id and text columns, for Select2
             html_options[:'data-select-options'] = column_select_options[column].map { |id, text|
-              {:id => id, :text => text}
+              {id: id, text: text}
             }.to_json
           end
 
@@ -185,7 +185,7 @@ module Ransack
           if foreign_klass
             # If field is a foreign key, set up 'data-ajax-*' attributes for auto-complete
             controller = ActiveSupport::Inflector::tableize(foreign_klass.to_s)
-            html_options[:'data-ajax-entity'] = I18n.translate(controller, :default => controller)
+            html_options[:'data-ajax-entity'] = I18n.translate(controller, default: controller)
             if ajax_options[:url]
               html_options[:'data-ajax-url'] = ajax_options[:url].sub(':controller', controller)
             else
@@ -221,7 +221,7 @@ module Ransack
           next nil if base.blank? && column == 'id'
 
           attribute = attr_from_base_and_column(base, column)
-          attribute_label = Translate.attribute(attribute, :context => object.context)
+          attribute_label = Translate.attribute(attribute, context: object.context)
 
           # Set model name as label for 'id' column on that model's table.
           if column == 'id'
@@ -229,7 +229,7 @@ module Ransack
             # Check that model can autocomplete. If not, skip this id column.
             next nil unless ActiveSupport::Inflector::constantize(foreign_klass.to_s)._ransack_can_autocomplete
 
-            attribute_label = I18n.translate(foreign_klass, :default => foreign_klass)
+            attribute_label = I18n.translate(foreign_klass, default: foreign_klass)
           else
             foreign_klass = foreign_keys[column.to_sym]
           end
